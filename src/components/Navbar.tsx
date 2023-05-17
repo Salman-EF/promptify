7e40e314505a7bced30bc210a73c0cd123c1e45f
SALMAN <elfadili.salman@gmail.com>
2023-05-17 19:44:28 +0100

    search input max width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, {  } from 'react';
-import { AppBar, Toolbar, Button, Stack } from '@mui/material';
+import { AppBar, Button, Stack } from '@mui/material';
 import logo from '../assets/img/logo.png';
 import SearchInput from './SearchInput';
 
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
 
   return (
     <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
-      <Stack direction={'row'} alignItems={'center'} spacing={4}>
+      <Stack direction={'row'} alignItems={'center'} spacing={4} justifyContent={'space-evenly'}>
         <img src={logo} alt="Logo" style={{ height: '76px', marginRight: '10px' }} />
         <Button>Explore</Button>
         <Button>Learn</Button>
