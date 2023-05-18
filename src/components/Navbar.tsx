959c8a9962d5fbbc13eeca099dbb779b369b664d
SALMAN <elfadili.salman@gmail.com>
2023-05-18 01:49:48 +0100

    appbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, {  } from 'react';
-import { AppBar, Button, Stack } from '@mui/material';
+import { AppBar, Link, Stack } from '@mui/material';
 import logo from '../assets/img/logo.png';
 import SearchInput from './SearchInput';
 
@@ -9,11 +9,11 @@ const Navbar: React.FC = () => {
     <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
       <Stack direction={'row'} alignItems={'center'} spacing={4} justifyContent={'space-evenly'}>
         <img src={logo} alt="Logo" style={{ height: '76px', marginRight: '10px' }} />
-        <Button>Explore</Button>
-        <Button>Learn</Button>
+        <Link href='#'>Explore</Link>
+        <Link href='#'>Learn</Link>
         <SearchInput />
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+        <Link href='#'>Sign In</Link>
+        <Link href='#' className='primary'>Sign Up</Link>
       </Stack>
     </AppBar>
   );
