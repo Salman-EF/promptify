07eb9195ed6f6438912f881e770d42d28133a159
SALMAN <elfadili.salman@gmail.com>
2023-05-18 13:37:26 +0100

    prompt card is a link

--- src/components/PromptCard.tsx
+++ src/components/PromptCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Card, CardMedia, CardContent, Box } from '@mui/material';
+import { Typography, Card, CardMedia, CardContent, Box, Link } from '@mui/material';
 import { Prompt } from '../utils/constants';
 interface PromptCardProps {
   prompt: Prompt;
@@ -8,21 +8,23 @@ interface PromptCardProps {
 const PromptCard: React.FC<PromptCardProps> = ({ prompt }) => {
   return (
     <Box display="flex" justifyContent="center">
-      <Card>
+      <Link href="#">
+      <Card className="prompt-card">
          <CardMedia component="img" image={prompt.imageSrc} alt={prompt.title} 
             className="prompt-img" 
          />
          <CardContent>
-            <Typography variant="h6">{prompt.title}</Typography>
+            <Typography variant="h6" className="prompt-ttl">{prompt.title}</Typography>
             <Typography variant="body2">
                <div className='topics'>
                   {prompt.topics.map((topic, i) => (
-                     <span key={i}>{topic}</span>
+                     <span key={i} className="topic">{topic}</span>
                   ))}
                </div>
             </Typography>
          </CardContent>
       </Card>
+      </Link>
     </Box>
   );
 };
